78d048da84f00a97a49f17a3133d6f6282b36d00
KeyaRaniMondal <keyamondal549@gmail.com>
2026-02-21 14:09:58 +0600

    completes basic mongodb setup

--- Backend/Server.js
+++ Backend/Server.js
@@ -1,11 +1,16 @@
 import express from 'express';
-const app=express();
+import dotenv from 'dotenv';
+import { connectToDB } from './config/db.js';
 
-const PORT=5000;
+const app=express();
+dotenv.config();
+
+const PORT=process.env.PORT || 5000;
 app.get("/",(req,res)=>{
     res.send("Hello World");
 })
 
 app.listen(PORT,()=>{
+    connectToDB();
     console.log(`server is running on port http://localhost:${PORT}`)
 })
--- README.md
+++ README.md
@@ -8,3 +8,8 @@ This is a mern stack project where I'm using THDB to get all the apis for movies
 
 - Movie Details with Recommendations
 ![Movie details](<localhost_5173_movie_1084242 (1).png>)
+
+
+
+- backend run using: node Server.js (problem : we need to start our server after every update) to solve this we need to use nodemon
+- dotenv to use env files
