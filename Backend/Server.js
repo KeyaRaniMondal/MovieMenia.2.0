00e9698aa2bbcc16dcef3d2ee89f71be985bc653
KeyaRaniMondal <keyamondal549@gmail.com>
2026-03-07 21:24:14 +0600

    fixed mongodb connection issue

--- Backend/Server.js
+++ Backend/Server.js
@@ -1,3 +1,8 @@
+//force dns to use ipv4 addresses
+import server from 'node:dns/promises' //for esm modules
+// const ser=require('node:dns/promises')  for common.js
+server.setServers(['1.1.1.1','8.8.8.8'])
+
 import express from 'express';
 import dotenv from 'dotenv';
 import { connectToDB } from './config/db.js';
@@ -14,3 +19,22 @@ app.listen(PORT,()=>{
     connectToDB();
     console.log(`server is running on port http://localhost:${PORT}`)
 })
+
+
+
+
+
+// const express = require("express");
+// const cors=require("cors");
+// const mongoose = require("mongoose");
+// require("dotenv").config();
+// const connectDB = require("./config/db");
+// const { MongoClient, ServerApiVersion } = require('mongodb');
+// const PORT=process.env.PORT || 5000;
+// const app = express();
+
+// // Middleware
+// app.use(cors());
+// app.use(express.json());
+
+
--- Backend/config/db.js
+++ Backend/config/db.js
@@ -9,3 +9,20 @@ export async function connectToDB() {
         console.log("Error connecting to DB", error);
     }
 }
+
+
+
+// // 
+// import mongoose from 'mongoose';
+
+// const connectDB = async () => {
+//   try {
+//     const conn = await mongoose.connect(process.env.MONGO_URI);
+//     console.log('MongoDB connected:', conn.connection.host);
+//   } catch (err) {
+//     console.error('MongoDB connection failed', err);
+//     process.exit(1);
+//   }
+// };
+
+// export default connectDB;
