ab3ddf7c4b6c0ce705ce2aa9cfb4665e23f5e7e2
KeyaRaniMondal <keyamondal549@gmail.com>
2026-03-09 22:02:55 +0600

    created usermodel schema

--- Backend/Server.js
+++ Backend/Server.js
@@ -5,12 +5,42 @@ server.setServers(['1.1.1.1','8.8.8.8'])
 
 import express from 'express';
 import dotenv from 'dotenv';
+import cors from 'cors';
 import { connectToDB } from './config/db.js';
+import User from './models/User.js';
 
 const app=express();
+app.use(cors()); // Enable CORS for all routes
+app.use(express.json());//middleware to parse json data from request body
 dotenv.config();
 
 const PORT=process.env.PORT || 5000;
+
+app.post('/api/signup',async (req,res)=>{
+  const {username,email,password}=req.body;
+//   console.log("Received signup data:", { username, email, password });
+try{
+    if(!username || !email || !password){
+        throw new Error("All fields are required")
+    }
+    const emailExists=await User.findOne({email});
+    if(emailExists){
+        return res.status(400).json({message:"Email already exists"})
+    }
+
+    const usernameExists=await User.findOne({username});
+    if(usernameExists){
+        return res.status(400).json({message:"Username is taken, try anotheer one"})
+    }
+    res.status(200).json({ message: "Signup successful", user: { username, email } });
+}
+  catch(error){
+    console.error("Error during signup:", error);
+    res.status(500).json({ message: "Internal server error" });
+  }
+  
+})
+
 app.get("/",(req,res)=>{
     res.send("Hello World");
 })
