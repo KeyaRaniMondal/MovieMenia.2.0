28f7b3095ae89d6fc73f382e6658ed9bc2ade6bb
KeyaRaniMondal <keyamondal549@gmail.com>
2026-03-12 21:19:26 +0600

    created login route

--- Backend/Server.js
+++ Backend/Server.js
@@ -18,6 +18,7 @@ dotenv.config();
 
 const PORT = process.env.PORT || 5000;
 
+// signup route
 app.post('/api/signup', async (req, res) => {
     const { username, email, password } = req.body;
     //   console.log("Received signup data:", { username, email, password });
@@ -42,7 +43,7 @@ app.post('/api/signup', async (req, res) => {
             email,
             password: hashedPassword
         })
-        
+
         //jwt token creation and sending it as a cookie to the client
         if (userDoc) {
             const token = jwt.sign({ id: userDoc._id }, process.env.JWT_SECRET, { expiresIn: "5d" })
@@ -60,6 +61,38 @@ app.post('/api/signup', async (req, res) => {
         res.status(500).json({ message: "Internal server error" });
     }
 
+})
+
+// login route
+app.post('/api/login', async (req, res) => {
+    const { username, password } = req.body
+    try {
+        const userDoc = await User.findOne({ username })
+        if (!userDoc) {
+            return res.status(400).json({ message: "Invalid username or password" })
+        }
+        const isPasswordValid = await bcrypt.compareSync(
+            password,
+            userDoc.password
+        )
+        if (!isPasswordValid) {
+            return res.status(400).json({ message: "Invalid password" })
+        }
+        if (userDoc) {
+            const token = jwt.sign({ id: userDoc._id }, process.env.JWT_SECRET, { expiresIn: "5d" })
+
+            res.cookie("token", token, {
+                httpOnly: true,
+                secure: process.env.NODE_ENV === "production",
+                sameSite: "strict",
+            })
+        }
+        res.status(200).json({ user: userDoc, message: "User LoggedIn successfully" });
+    }
+    catch (error) {
+        console.error("Error during login:", error);
+        res.status(500).json({ message: "Internal server error" });
+    }
 })
 
 app.get("/", (req, res) => {
