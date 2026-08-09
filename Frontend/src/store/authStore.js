eeaeb9f73940869546246845822f3a1c71eb4f84
KeyaRaniMondal <keyamondal549@gmail.com>
2026-08-09 21:32:49 +0600

    updated api url after deploy

--- Frontend/src/store/authStore.js
+++ Frontend/src/store/authStore.js
@@ -2,7 +2,7 @@ import { create } from "zustand";
 import axios from "axios";
 
 axios.defaults.withCredentials = true; // Enable sending cookies with requests
-const API_URL = import.meta.env.VITE_API_URL || "";
+const API_URL = import.meta.env.VITE_API_URL || "http://localhost:5000";
 
 export const useAuthStore = create((set) => ({
   user: null,
